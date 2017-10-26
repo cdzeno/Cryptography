f1a4f69d94bf16386bd905079885fea4841734c4
Gabriele Quagliarella <quaggigabri@gmail.com>
2017-10-26 21:10:16 +0200

    Update des.c

--- des.c
+++ des.c
@@ -16,8 +16,24 @@ typedef struct _Key
   unsigned int key: 9;
 } Key;
 
+typedef struct _InSBox{
+  unsigned int left:4;
+  unsigned int right:4;
+} InSBox;
+
+typedef struct _OutSBox{
+  unsigned int left:3;
+  unsigned int right:3;
+} OutSBox;
+
+typedef struct _CoupleKey{
+  unsigned int left:4;
+  unsigned int right:4;
+} CoupleKey;
+
 uint8_t s1 [2][8] = {{5,2,1,6,3,4,7,0},{1,4,6,2,0,7,5,3}};
 uint8_t s2 [2][8] = {{4,0,6,5,7,1,3,2},{5,3,0,7,6,2,1,4}};
+Key key;
 
 uint8_t expansion (uint8_t r0); //in: 6bit, out: 8bit
 uint8_t getBox (uint8_t sbox[2][8], uint8_t in_sbox); //in: mat3bit 4bit, out: 3bit
@@ -25,7 +41,10 @@ uint8_t feistel (uint8_t r0, uint8_t key); //in: 6bit 8bit, out: 6bit
 L0R0 turn (L0R0 input, uint8_t key); //in: L0R0 8bit, out: L0R0
 uint8_t keySchedule (Key key, uint8_t n_round);//in: 9bit 4bit, out: 8bit
 unsigned int bin_string_to_int (char* s); //in: string binary digit out: unsigned int
-void des (L0R0 plaintext, Key key, uint64_t n_round);
+L0R0 des (L0R0 plaintext, Key key, uint64_t n_round);
+uint8_t* diff_crypto (L0R0 p1, L0R0 p2, L0R0 c1, L0R0 c2);
+uint8_t** gen_key (L0R0* lista_plain);
+void get_key(uint8_t** list_key);
 
 uint8_t expansion (uint8_t r0)
 {
@@ -65,7 +84,7 @@ uint8_t keySchedule (Key key, uint8_t n_round)
   return ( (key.key << n_round) | ((key.key & bitmask.key) >> (uint16_t)(9 - n_round)) );
 }
 
-void des (L0R0 plaintext, Key key, uint64_t n_round)
+L0R0 des (L0R0 plaintext, Key key, uint64_t n_round)
 {
   L0R0 input = plaintext;
 
@@ -77,8 +96,8 @@ void des (L0R0 plaintext, Key key, uint64_t n_round)
     input = turn(input,roundkey);
     //printf("output %d: left=0x%x  right=0x%x\n", (int)i+1, input.l0,input.r0);
   }
-  printf("Ciphertext: left=0x%x  right=0x%x\n", input.l0, input.r0);
-
+  //printf("Ciphertext: left=0x%x  right=0x%x\n", input.l0, input.r0);
+  return input;
 }
 
 unsigned int bin_string_to_int (char* s){
@@ -94,25 +113,103 @@ unsigned int bin_string_to_int (char* s){
   return binary;
 }
 
+uint8_t* diff_crypto (L0R0 p1, L0R0 p2, L0R0 c1, L0R0 c2){
+  InSBox in = {.left = (expansion(c1.l0) ^ expansion(c2.l0)) >> 4,
+               .right = (expansion(c1.l0) ^ expansion(c2.l0)) & 0b00001111};
+
+  OutSBox out = {.left = ((c1.r0 ^ c2.r0) ^ (p1.l0 ^ p2.l0)) >> 3,
+                 .right =   ((c1.r0 ^ c2.r0) ^ (p1.l0 ^ p2.l0)) & 0b000111};
+
+  //printf("InSbox: left=0x%x | right=0x%x\n", in.left, in.right);
+  //printf("OutSbox: left=0x%x | right=0x%x\n", out.left, out.right);
+  // Bruteforce S1: 0000 => 1111
+  CoupleKey* key_list1 = malloc(sizeof(CoupleKey)*16);
+  CoupleKey* key_list2 = malloc(sizeof(CoupleKey)*16);
+
+  int index1 = 0;
+  int index2 = 0;
+
+  for (uint8_t i = 0b0000; i <= 0b1111 ; i++) {
+    if((getBox(s1,i) ^ getBox(s1, (i ^ in.left))) == out.left){
+      key_list1[index1].left = i;
+      key_list1[index1].right = i ^ in.left;
+      index1++;
+    }
+  }
+
+  // Bruteforce S2: 0000 => 1111
+  for (uint8_t i = 0b0000; i <= 0b1111 ; i++) {
+    if((getBox(s2,i) ^ getBox(s2, (i ^ in.right))) == out.right){
+      key_list2[index2].left = i;
+      key_list2[index2].right = i ^ in.right;
+      index2++;
+    }
+  }
+
+  uint8_t* LastKey = malloc(sizeof(uint8_t)*((index1*index2) + 1));
+  int index3 = 0;
+
+  for (int i = 0; i < index1; i++) {
+    for (int j = 0; j < index2; j++) {
+      LastKey[index3] = (((uint8_t)(key_list1[i].left)) << 4) | key_list2[j].right;
+      printf("LastKey[%d]: 0x%x\n", index3, LastKey[index3]);
+      index3++;
+    }
+  }
+
+  LastKey[index3] = '0x0'
+  return LastKey;
+}
+
+uint8_t** gen_key (L0R0* lista){
+  // length Lista = 8
+  uint8_t** list_key = malloc(sizeof(uint8_t*)*56);
+  int index = 0;
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if(i != j){
+          list_key[index] = diff_crypto(lista[i], lista[j], des(lista[i], key, 3), des(lista[j], key, 3));
+          index++;
+      }
+    }
+  }
+  return list_key;
+}
+
+void print_gen_key (uint8_t** gen_key){
+  uint8_t** r = gen_key;
+  uint8_t* c = gen_key[0];
+
+  while(*c != ){
+
+  }
+
+}
+
 int main(int argc, char* argv[])
 {
   // argv[1] = 12 bit plaintext
   // argv[2] = 9 bit key
-  assert(argc == 3);
-  assert((int)strlen(argv[1]) == 12 && (int)strlen(argv[2]) == 9);
+  assert(argc == 2);
+  assert((int)strlen(argv[1]) == 9);
+  //assert((int)strlen(argv[1]) == 12 && (int)strlen(argv[2]) == 9);
 
   char l0[7] = {'0','0','0','0','0','0','\0'};
   char r0[7] = {'0','0','0','0','0','0','\0'};
-  char key[10] = {'0','0','0','0','0','0','0','0','0','\0'};
-  (void)strncpy(l0, argv[1], 6);
-  (void)strncpy(r0, argv[1]+6, 6);
-  (void)strncpy(key, argv[2], 9);
+  char key_string[10] = {'0','0','0','0','0','0','0','0','0','\0'};
+  //(void)strncpy(l0, argv[1], 6);
+  //(void)strncpy(r0, argv[1]+6, 6);
+  (void)strncpy(key, argv[1], 9);
 
-  printf("Plaintext: l0=0b%s | r0=0b%s\nKey: 0b%s\n", l0, r0, key);
+  printf("Key: 0b%s\n", key_string);
 
-  L0R0 plain = {.l0 = bin_string_to_int(l0), .r0 = bin_string_to_int(r0)};
-  Key key_plain = {.key = bin_string_to_int(key)};
-
-  des(plain, key_plain, 3);
+  //L0R0 plain1 = {.l0 = bin_string_to_int(l0), .r0 = bin_string_to_int(r0)};
+  //L0R0 plain2 = {.l0 = 0b000111, .r0 = 0b011011};
+  key = {.key = bin_string_to_int(key_string)};
+  //L0R0 cipher1 = des(plain1, key_plain, 3);
+  //L0R0 cipher2 = des(plain2, key_plain, 3);
+  //diff_crypto(plain1, plain2, cipher1, cipher2);
+  gen_key();
   return 0;
 }
